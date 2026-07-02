031847ab5155b8a8a8a35ed582740ebd05340ba0
huangikaiaa3 <huangikaiaa3@gmail.com>
2026-07-02 12:56:44 +0200

    Fix storage volume permissions on EC2

--- docker/api/start.sh
+++ docker/api/start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
 
+mkdir -p /app/storage /app/storage/documents
+chown -R appuser:appuser /app/storage
+
 python -m alembic upgrade head
 
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
--- docker/worker/start.sh
+++ docker/worker/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -eu
 
+mkdir -p /app/storage /app/storage/documents
+chown -R appuser:appuser /app/storage
+
 exec python -m app.workers.ingestion_worker
